b9214be45d4322a7c5e78137f25bbbf278aebb23
James Wolfe <james.wolfe.2023@student.lhschools.org>
2020-03-02 12:31:50 -0500

    Commit 3/2/2020 12:31 PM

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,15 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	
+        methodName();
+
+        public static void methodName(testOddness); {
+
+
+        }
     }
 }
