47f186ee302b85790a11c2e5c2a84cbaa4c0c243
James Wolfe <james.wolfe.2023@student.lhschools.org>
2020-03-05 11:15:30 -0500

    OddnessMethodCreated Updated

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -4,8 +4,16 @@ import java.util.Scanner;
 
 public class Main {
 
+
     public static void main(String[] args) {
+
         testOddness();
+
+//        keepContained();
+
+    }
+
+    public static void testOddness() {
 
         int number;
 
@@ -17,17 +25,23 @@ public class Main {
 
         number = keyboard.nextInt();
 
-        System.out.println("Odd: " + (number % 2 != 0));
-
-        System.out.println("Even: " + (number % 2 == 0));
-
+        System.out.println("Number is odd: " + (number % 2 != 0));
 
     }
 
-    public static void testOddness() {
+    public static void keepContained() {
 
+        int number;
 
+        Scanner keyboard;
 
+        keyboard = new Scanner(System.in);
+
+        System.out.println("Give me a positive number: ");
+
+        number = keyboard.nextInt();
+
+        System.out.println(number);
 
         }
 
