a5b90bdf273b31f193d9b2c6c8465c46f61f969e
James Wolfe <james.wolfe.2023@student.lhschools.org>
2020-03-05 11:13:34 -0500

    OddnessMethodCreated

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -6,10 +6,21 @@ public class Main {
 
     public static void main(String[] args) {
         testOddness();
+
         int number;
+
         Scanner keyboard;
-        keyboard.nextLine();
-        System.out.println("This is a postive number" + keyboard);
+
+        keyboard = new Scanner(System.in);
+
+        System.out.println("This number is: ");
+
+        number = keyboard.nextInt();
+
+        System.out.println("Odd: " + (number % 2 != 0));
+
+        System.out.println("Even: " + (number % 2 == 0));
+
 
     }
 
